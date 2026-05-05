34f342c58aee2bd8124120f19cd33c229bea6f4d
Aviferdev <franciscojaviferavifer@gmail.com>
2026-05-05 20:29:00 +0200

    Fix

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -62,7 +62,7 @@ sqldelight {
     databases {
         create("TrackfolioDatabase") {
             packageName.set("es.aviferdev.trackfolio.data.database")
-            version = 5
+            version = 6
         }
     }
 }
